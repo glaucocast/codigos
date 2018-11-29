cad2f52045b46ba101cfbf0fad6aeec4ef16ac2c
Glauco Castro <glaucocastro53@gamail.com>
2018-11-28 23:40:51 -0600

    laravel

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+@section('modulo')
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
---
+++ storage/framework/views/ea3cc9a993973a8c71071beebf0a93f912214740.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('modulo'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
